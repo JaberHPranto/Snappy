1297ecbeb0286600e2250c563ca59b8fb822fa5d
JaberHPranto <jabearhossain@iut-dhaka.edu>
2022-08-23 23:30:21 +0600

    🖼️ implement resize functionality

--- client/src/components/Whiteboard/NewWhiteboard.jsx
+++ client/src/components/Whiteboard/NewWhiteboard.jsx
@@ -4,8 +4,9 @@ import { useLayoutEffect } from "react";
 import rough from "roughjs";
 
 const roughGenerator = rough.generator();
+const THRESHOLD = 5;
 
-const createElement = (x1, y1, x2, y2, type, options = {}) => {
+const createElement = (id, x1, y1, x2, y2, type, options = {}) => {
   const roughElement =
     type === "line"
       ? roughGenerator.line(x1, y1, x2, y2, options)
@@ -21,14 +22,137 @@ const createElement = (x1, y1, x2, y2, type, options = {}) => {
           options
         )
       : roughGenerator.rectangle(x1, y1, x2 - x1, y2 - y1, options);
-  return { x1, y1, x2, y2, roughElement };
+  return { id, x1, y1, x2, y2, roughElement, type, options };
+};
+
+const getPosition = (x, y, elements) => {
+  return elements
+    .map((el) => {
+      return {
+        ...el,
+        position: positionWithinBoundary(x, y, el),
+      };
+    })
+    .find((el) => el.position !== null);
+};
+
+const positionWithinBoundary = (x, y, element) => {
+  const { type, x1, x2, y1, y2 } = element;
+  if (type === "rectangle") {
+    // for resizing
+    const topLeft = nearPosition(x, y, x1, y1, "tl");
+    const topRight = nearPosition(x, y, x2, y1, "tr");
+    const bottomLeft = nearPosition(x, y, x1, y2, "bl");
+    const bottomRight = nearPosition(x, y, x2, y2, "br");
+    const isInside = x1 <= x && x <= x2 && y2 >= y && y1 <= y ? "inside" : null;
+    return topLeft || topRight || bottomLeft || bottomRight || isInside;
+  } else if (type === "line") {
+    const a = { x: x1, y: y1 };
+    const b = { x: x2, y: y2 };
+    const c = { x, y };
+    // for resizing
+    const start = nearPosition(x, y, x1, y1, "start");
+    const end = nearPosition(x, y, x2, y2, "end");
+    const offset = distance(a, b) - (distance(a, c) + distance(b, c));
+    const isInside =
+      Math.abs(offset) < 1 || (x2 > x && x1 < x && y2 > y && y1 < y)
+        ? "inside"
+        : null;
+    return start || end || isInside;
+  } else if (type === "circle") {
+    const r = ((x2 * x2) / (8 * y2) + y2 / 2) / 4;
+    const w = (x2 - x1) * 2;
+    const h = (y2 - y1) * 2;
+    const left = nearPosition(x, y, x1 + w / 2, y1, "lt");
+    const right = nearPosition(x, y, x1 - w / 2, y1, "rt");
+    const top = nearPosition(x, y, x1, y1 + h / 2, "tp");
+    const bottom = nearPosition(x, y, x1, y1 - h / 2, "bm");
+    const isInside =
+      Math.pow(x - x1, 2) + Math.pow(y - y1, 2) < Math.pow(r, 2)
+        ? "inside"
+        : null;
+    return left || right || top || bottom || isInside;
+  }
+};
+
+const distance = (a, b) => {
+  return Math.sqrt(Math.pow(a.x - b.x, 2) - Math.pow(a.y - b.y, 2));
+};
+
+const nearPosition = (x, y, x1, y1, position) => {
+  return Math.abs(Math.abs(x - x1) < THRESHOLD && Math.abs(y - y1) < THRESHOLD)
+    ? position
+    : null;
+};
+
+const getCursorForPosition = (position) => {
+  switch (position) {
+    case "tl":
+    case "br":
+      return "nwse-resize";
+    case "tr":
+    case "bl":
+      return "nesw-resize";
+    case "rt":
+    case "start":
+    case "lt":
+    case "end":
+      return "w-resize";
+    case "tp":
+    case "bm":
+      return "n-resize";
+    default:
+      return "move";
+  }
+};
+
+// so that all co-ordinates remain consistent irrespective of how we draw -> (x1,y1) <= (x2,y2).It helps for resizing
+const adjustElementCoordinates = (element) => {
+  const { x1, y1, x2, y2, type } = element;
+  if (type === "rectangle") {
+    const minX = Math.min(x1, x2);
+    const maxX = Math.max(x1, x2);
+    const minY = Math.min(y1, y2);
+    const maxY = Math.max(y1, y2);
+
+    return { x1: minX, y1: minY, x2: maxX, y2: maxY };
+  } else if (type === "line") {
+    if (x1 < x2 || (x1 === x2 && y1 < y2)) return { x1, y1, x2, y2 };
+    else return { x1: x2, y1: y2, x2: x1, y2: y1 };
+  }
+};
+
+const resizeCoordinates = (mouseX, mouseY, position, coordinates) => {
+  const { x1, y1, x2, y2 } = coordinates;
+  switch (position) {
+    case "tl":
+    case "start":
+      return { x1: mouseX, y1: mouseY, x2, y2 };
+    case "tr":
+      return { x1, y1: mouseY, x2: mouseX, y2 };
+    case "bl":
+      return { x1: mouseX, y1, x2, y2: mouseY };
+    case "br":
+    case "end":
+      return { x1, y1, x2: mouseX, y2: mouseY };
+    case "tp":
+    case "bm":
+      return { x1, y1, x2, y2: mouseY };
+    case "rt":
+    case "lt":
+      return { x1, y1, x2: mouseX, y2 };
+    default:
+      return { x1, y1, x2, y2 };
+  }
 };
 
 const NewWhiteboard = () => {
   const [elements, setElements] = useState([]);
-  const [drawing, setDrawing] = useState(false);
+  const [action, setAction] = useState("none");
   const [tool, setTool] = useState("line");
   const [color, setColor] = useState("#000000");
+  const [selectedElement, setSelectedElement] = useState(null);
+
   const canvasRef = useRef();
   const ctxRef = useRef();
 
@@ -39,6 +163,7 @@ const NewWhiteboard = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     const roughCanvas = rough.canvas(canvas);
+    ctx.lineCap = "arrow";
 
     elements.forEach((el) => {
       roughCanvas.draw(el.roughElement);
@@ -51,28 +176,91 @@ const NewWhiteboard = () => {
 
   const handleMouseDown = (e) => {
     const { clientX, clientY } = e;
-    const element = createElement(clientX, clientY, clientX, clientY, tool, {
-      stroke: color,
-    });
-    setElements((prevElements) => [...prevElements, element]);
-    setDrawing(true);
+    if (tool === "selection") {
+      const element = getPosition(clientX, clientY, elements);
+      if (element) {
+        const offsetX = clientX - element.x1;
+        const offsetY = clientY - element.y1;
+        setSelectedElement({ ...element, offsetX, offsetY });
+        if (element.position === "inside") {
+          setAction("moving");
+        } else setAction("resizing");
+      }
+    } else {
+      // drawing
+      const id = elements.length;
+      const element = createElement(
+        id,
+        clientX,
+        clientY,
+        clientX,
+        clientY,
+        tool,
+        {
+          stroke: color,
+          strokeWidth: 2,
+        }
+      );
+      setElements((prevElements) => [...prevElements, element]);
+      setSelectedElement(element);
+      setAction("drawing");
+    }
   };
   const handleMouseMove = (e) => {
     const { clientX, clientY } = e;
-    if (drawing) {
-      const lastElement = elements.length - 1;
-      const { x1, y1 } = elements[lastElement];
-      const updatedElement = createElement(x1, y1, clientX, clientY, tool, {
+    if (tool === "selection") {
+      const element = getPosition(clientX, clientY, elements);
+      e.target.style.cursor = element
+        ? getCursorForPosition(element.position)
+        : "default";
+    }
+    if (action === "drawing") {
+      const index = selectedElement.id;
+      const { x1, y1, id } = elements[index];
+      updatedElement(id, x1, y1, clientX, clientY, tool, {
         stroke: color,
+        strokeWidth: 2,
       });
+    } else if (action === "moving") {
+      const { id, x1, y1, x2, y2, type, offsetX, offsetY, options } =
+        selectedElement;
+      const width = x2 - x1;
+      const height = y2 - y1;
+      const newX = clientX - offsetX;
+      const newY = clientY - offsetY;
 
-      const allElements = [...elements];
-      allElements[lastElement] = updatedElement;
-      setElements(allElements);
+      updatedElement(
+        id,
+        newX,
+        newY,
+        newX + width,
+        newY + height,
+        type,
+        options
+      );
+    } else if (action === "resizing") {
+      const { id, type, position, options, ...coordinates } = selectedElement;
+      const { x1, y1, x2, y2 } = resizeCoordinates(
+        clientX,
+        clientY,
+        position,
+        coordinates
+      );
+      updatedElement(id, x1, y1, x2, y2, type, options);
     }
   };
   const handleMouseUp = () => {
-    setDrawing(false);
+    const element = elements[elements.length - 1];
+    const { id, type } = element;
+    if (action === "drawing" && type !== "circle") {
+      const { x1, y1, x2, y2 } = adjustElementCoordinates(element);
+      updatedElement(id, x1, y1, x2, y2, type, {
+        stroke: color,
+        strokeWidth: 2,
+      });
+    }
+    setAction("none");
+    setSelectedElement(null);
   };
 
   const handleClearCanvas = () => {
@@ -83,6 +271,14 @@ const NewWhiteboard = () => {
       canvasRef.current.height
     );
     setElements([]);
+  };
+
+  const updatedElement = (id, x1, y1, x2, y2, type, options = {}) => {
+    const updatedElement = createElement(id, x1, y1, x2, y2, type, options);
+
+    const allElements = [...elements];
+    allElements[id] = updatedElement;
+    setElements(allElements);
   };
 
   return (
@@ -129,6 +325,17 @@ const NewWhiteboard = () => {
               id="circle"
               checked={tool === "circle"}
               value="circle"
+              onChange={(e) => setTool(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="selection">Selection</label>
+            <input
+              type="radio"
+              name="tool"
+              id="selection"
+              checked={tool === "selection"}
+              value="selection"
               onChange={(e) => setTool(e.target.value)}
             />
           </div>
